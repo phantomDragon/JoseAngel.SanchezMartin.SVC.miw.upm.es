4407fa24ae600ef14efdb35ce90cff58e27b1d16
angel <bo0477@192.168.7.19>
2017-09-18 17:28:07 +0200

    Creada clase C31

---
+++ src/main/java/es/upm/miw/angelmart/junit/C31.java
@@ -0,0 +1,16 @@
+package es.upm.miw.angelmart.junit;
+
+public class C31 {
+
+    public C31() {
+        
+    }
+    
+    public String m1() {
+        return "C31.m1()"; 
+    }
+    
+    public String m2() {
+        return "C31.m2()"; 
+    }
+}
