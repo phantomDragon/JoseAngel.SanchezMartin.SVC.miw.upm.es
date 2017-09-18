aa17620e4ef8906b66b131bac10195bc43dac750
angel <bo0477@192.168.7.19>
2017-09-18 18:40:12 +0200

    Creada la clase C51

---
+++ src/main/java/es/upm/miw/angelmart/junit/C51.java
@@ -0,0 +1,17 @@
+package es.upm.miw.angelmart.junit;
+
+public class C51 {
+    
+    public C51() {
+        
+    }
+    
+    public String m1() {
+        return "C51.m1()"; 
+    }
+    
+    public String m2() {
+        return "C51.m2()"; 
+    }
+
+}
