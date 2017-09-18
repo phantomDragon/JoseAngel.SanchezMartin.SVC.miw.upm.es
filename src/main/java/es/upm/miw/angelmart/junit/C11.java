48a22ea5a3d42ec094a86093eb1f13d09746e96f
angel <bo0477@192.168.7.19>
2017-09-18 17:05:14 +0200

    Creada clase C11

---
+++ src/main/java/es/upm/miw/angelmart/junit/C11.java
@@ -0,0 +1,17 @@
+package es.upm.miw.angelmart.junit;
+
+public class C11 {
+    
+    public C11() {
+        
+    }
+    
+    public String m1() {
+        return "C11.m1()"; 
+    }
+    
+    public String m2() {
+        return "C11.m2()"; 
+    }
+
+}
